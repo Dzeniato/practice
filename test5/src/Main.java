4dfb8a6ffa323d21d087c9ab1ad487af80cd5e66
Tash <ivanyman@mail.ru>
2018-10-25 10:17:46 +0700

    исправление первой лабы вар 5

--- test5/src/Main.java
+++ test5/src/Main.java
@@ -17,20 +17,21 @@ public class Main {
     static String func1(int year) {
         String a="";
         if (year >1918) {
-            if (year % 4 == 0) {
-                a = "12.09." + year;
+            if (((year % 4 == 0)&&(year % 100 != 0)&&(year % 400!=0))||(year % 400==0)) {
+                a = "visokosnii 12.09." + year;
             } else {
                 a = "13.09."+ year;
             }
         }else if (year < 1918) {
-            if ((year % 4 == 0) &&(year %400 ==0)) {
-                a = "12.09."+ year;
+            if (year % 4 == 0)  {
+                a = "visokosnii 12.09."+ year;
             } else {
                 a = "13.09."+ year;
             }
         }
         else if (year ==1918){
-            a = "31.08."+ year;
+            a = "26.09."+ year;
+
 
         }
         return  a;
